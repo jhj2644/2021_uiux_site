5a5009c15c4e5e7b395acd6113ada1469cd4ae03
jhj2644 <jhj2644@daum.net>
2021-09-13 18:19:06 +0900

    company

--- 05company copy/js/script.js
+++ 05company copy/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    $('.slider').bxSlider();
+    const slider1 = $('.slider').bxSlider({
+        controls:false
+    });
+    $(".nextBtn").click(function(){
+        slider1.goToNextSlide();
+    });
+    $(".prevBtn").click(function(){
+        slider1.goTopPrevSlide();
+    })
 });
