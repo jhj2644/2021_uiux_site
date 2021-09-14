40cbba7af9b365823d34a63e304fd8800e64b436
jhj2644 <jhj2644@daum.net>
2021-09-14 18:15:35 +0900

    paikdabang

--- paikdabang/js/script.js
+++ paikdabang/js/script.js
@@ -7,5 +7,22 @@ $(document).ready(function(){
     });
     $(".prevBtn").click(function(){
         slider1.goTopPrevSlide();
-    })
+    });
+
+    $(".toggle").click(function(){
+        const toggle =$(".toggle").attr("class");
+        console.log(toggle);
+        if( toggle =="toggle"){
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
 });
