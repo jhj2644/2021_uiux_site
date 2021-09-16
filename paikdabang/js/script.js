8f1679f03b232a2c8ccc966777e884df0807c246
jhj2644 <jhj2644@daum.net>
2021-09-16 14:04:04 +0900

    company

--- paikdabang/js/script.js
+++ paikdabang/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     const slider1 = $('.slider').bxSlider({
-        controls:false
+        controls:false,
+        auto:true
     });
     $(".nextBtn").click(function(){
         slider1.goToNextSlide();
